bcbfc534e8ab71ed6121391da59565fa8c75e896
kushal_dhar <kushal.dhar01@gmail.com>
2018-03-21 17:02:26 -0400

    Learning to use yargs

--- notes_2/app.js
+++ notes_2/app.js
@@ -8,5 +8,5 @@ const note = require('./note.js')
 console.log(_.isString(1));
 console.log(_.isString('Kushal'));
 
-var filteredArray = _.uniq([1,2,1,3,4,2]);
+var filteredArray = _.uniq([1,2,1,3,4,2,1,2]);
 console.log(filteredArray);
