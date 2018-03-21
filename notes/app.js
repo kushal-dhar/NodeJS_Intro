231cfee4a61292d4f12e0e5e9583c077243414c2
kushal_dhar <kushal.dhar01@gmail.com>
2018-03-21 15:37:02 -0400

    Learning to require own modules

--- notes/app.js
+++ notes/app.js
@@ -2,11 +2,15 @@ console.log('Starting app');
 
 const fs = require('fs');
 const os = require('os');
+const note = require('./note.js')
 
+var newnote = note.addNote();
 var user = os.userInfo();
-console.log(user)
+console.log(newnote);
 fs.appendFile('greetings.txt',`Hello ${user.username}!\n`, function (err) {
   if (err) {
     console.log('Unable to write to file');
   }
 });
+
+console.log(note.addNum(10,-3))
---
+++ notes/note.js
@@ -0,0 +1,10 @@
+console.log('Starting note.js');
+
+module.exports.addNote = () => {
+  console.log('Inside addnote function')
+  return 'New Note'
+}
+
+module.exports.addNum = (a,b) => {
+  return a + b
+}
