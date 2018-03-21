7e766e0e5a2360fcb972e75f437278d71866419d
kushal_dhar <kushal.dhar01@gmail.com>
2018-03-21 15:53:48 -0400

    Using third party lodash module

---
+++ notes_2/app.js
@@ -0,0 +1,12 @@
+console.log('Starting app');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+const note = require('./note.js')
+
+console.log(_.isString(1));
+console.log(_.isString('Kushal'));
+
+var filteredArray = _.uniq([1,2,1,3,4,2]);
+console.log(filteredArray);
